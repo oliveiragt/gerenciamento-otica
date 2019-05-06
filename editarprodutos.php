29fb7f4070b7f91cf742b4007371abf5dc342568
Gabriel Oliveira <gabriel.oliveirat@hotmail.com>
2019-05-06 17:48:33 -0300

    criado e finalizado CRUD produtos, sem atualização de menu

---
+++ editarprodutos.php
@@ -0,0 +1,16 @@
+<?php
+require 'conexao.php';
+$id=$_POST['id'];
+$referencia=$_POST['referencia'];
+$descricao=$_POST['descricao'];
+$unidade=$_POST['unidade'];
+$valor=$_POST['valor'];
+$quantidade=$_POST['quantidade'];
+$count=$dbn->query("UPDATE produtos SET referencia='$referencia',descricao='$descricao',unidade='$unidade',valor='$valor',quantidade='$quantidade' WHERE idproduto='$id'");
+if($count){
+    header("location:sucesso.php");
+}
+else{
+    header("location:falha.php");
+}
+?>
---
+++ listarprodutos.php
@@ -0,0 +1,133 @@
+<?php 
+require 'conexao.php';
+?>
+<!doctype html>
+<html lang="PT-BR">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./assets/img/oticaeva.jpg" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Lista de Produtos</title>
+</head>
+
+<body class="bg-light">
+    <div class="container-fluid">
+         <div class="row">
+            <div class="bg-warning col-sm-12">
+                <a href="sistema.php"><button type="button" class="btn btn-warning"><i
+                            class="fas fa-home"></i>Início</button></a>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Vendas</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Cadastrar Venda</a>
+                        <a class="dropdown-item" href="#">Listar Vendas</a>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Vendedores</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="cadvendedor.php">Cadastrar Vendedores</a>
+                        <a class="dropdown-item" href="listarvendedores.php">Listar Vendedores</a>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Produtos</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="cadproduto.php">Cadastrar Produtos</a>
+                        <a class="dropdown-item" href="#">Listar Produtos</a>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Usuários</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="cadusuario.php">Cadastrar Usuários</a>
+                        <a class="dropdown-item" href="listarusuarios.php">Listar Usuários</a>
+                    </div>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Relatórios</button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Completo</a>
+                        <a class="dropdown-item" href="#">Vendas</a>
+                        <a class="dropdown-item" href="#">Produtos vendidos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h2 class="text-center">Produtos Cadastrados</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <table class="table table-bordered table-hover table-sm">
+                    <thead class="thead-light">
+                        <th>Referência</th>
+                        <th>Descrição</th>
+                        <th>Unidade</th>
+                        <th>Valor</th>
+                        <th>Quantidade</th>
+                        <th colspan="2">Ações</th>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        $count=("SELECT * FROM produtos");
+                         foreach($dbn->query($count) as $row){
+                             switch($row['unidade']){
+                                case "pr":
+                                $unidade="Par";
+                                break;
+                                case "kt":
+                                $unidade="Kit";
+                                break;
+                                 case "pc":
+                                $unidade="Peça";
+                                break;
+                                 case "un":
+                                $unidade="Unidade";
+                                break;
+                             }
+                        ?>
+                        <tr>
+                            <td><?php echo $row['referencia']; ?></td>
+                            <td><?php echo $row['descricao']; ?></td>
+                            <td><?php echo $unidade; ?></td>
+                            <td><?php echo 'R$' . number_format($row['valor'], 2, ',', '.');?></td>
+                            <td><?php echo $row['quantidade'];?></td>
+                            <td><a title="Editar" href="editarproduto.php?id=<?php echo $row['idproduto']; ?>"><i class="far fa-edit"></i></a></td>
+                            <td><a title="Apagar" href="deletaproduto.php?id=<?php echo $row['idproduto']; ?>"><i class="far fa-trash-alt"></i></a></td>
+                        </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <a href="cadproduto.php"><button class="btn btn-outline-success">Cadastrar novo produto</button></a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
